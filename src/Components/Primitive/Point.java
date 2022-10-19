2741c79b94ef4e3ea19470677d0dd4f39a97b988
Stepan <St3Ba4@gmail.com>
2022-10-19 10:03:39 +0300

    Create Point

---
+++ src/Components/Primitive/Point.java
@@ -0,0 +1,13 @@
+package Components.Primitive;
+
+public class Point {
+    int x;
+    int y;
+    int z;
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
