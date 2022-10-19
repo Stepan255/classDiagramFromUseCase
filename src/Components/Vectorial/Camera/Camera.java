3720f208234ec5c62613aaae91a8f629b4eebcd8
Stepan <St3Ba4@gmail.com>
2022-10-19 10:05:55 +0300

    Create Camera

---
+++ src/Components/Vectorial/Camera/Camera.java
@@ -0,0 +1,12 @@
+package Components.Vectorial.Camera;
+
+import Components.Primitive.Point;
+import Components.Vectorial.Direction;
+import Components.Vectorial.Vectorial;
+
+public class Camera extends Vectorial {
+
+    public Camera(Point location, Direction direction) {
+        super(location, direction);
+    }
+}
